4f8822f2004c59eff8a30f5d05ac0aa8eb77881d
Ajit kumar chaurasiya <ajitchaurasia53@gmail.com>
2025-04-21 14:39:15 +0000

    Datatypes_firstLecture

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //code should be execute in strictMode. Treat all js code as newer version
+
+//alert(3+3); //ReferenceError: alert is not defined. We are using Node as Compiler not a browser Js compiler
+
+// console.log(3+
+//     3) // code Readability should be readable;
+
+let a= "4"+3;
+console.log(typeof a);
+
+//Ecma script write standard, rules, regulation for javascript;
+
+// =====================================
+// Primitive Datatypes
+//boolean => true/false
+//number => JavaScript can accurately represent integers within the range of -2^53 + 1 to 2^53 - 1 (inclusive).
+//bigint
+//string => "" alphabetic
+// undefined => : Represents a variable that has been declared but has not been assigned a value.
+//null => is standalone value if different from 0 or empty string; of type of object; null is a primitive value that represents the intentional absence of any object value.
+//symbol => Represents a unique and immutable value, often used as object property keys.
+
+
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); //object
+
+
+
+
