175668ac00983bf8b4887950439c8518b55144db
Ajit kumar chaurasiya <ajitchaurasia53@gmail.com>
2025-04-22 09:17:02 +0000

    04_comparision.js

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1); true
+// console.log(2 >= 1); true
+// console.log(2 < 1); false
+// console.log(2 == 1); false
+// console.log(2 != 1); true
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log("5"+3); //53 (+) have concate power with string
+console.log("5"-3); //2 
+console.log("5"*3); //15
+console.log("5"/3); //1.666
+console.log("5"%3); //2
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2); // check value with Datatypes
